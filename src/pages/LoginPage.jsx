4160b0ff6a3dafca33613b4a23d4bdba51f48565
KUNAL JAIN <kunaljainjain193@gmail.com>
2025-09-14 02:14:09 +0530

    added loginPage css

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'; 
 import { Link, useNavigate } from 'react-router-dom'; 
+import "./LoginPage.css";
 
 export default function LoginPage({ setIsLoggedIn }) {
   const navigate = useNavigate();
